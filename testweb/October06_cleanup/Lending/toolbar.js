a4dfac836a871e249cacf87a171a640f7a78672f
Alex Merrill <alexmerrill@users.noreply.github.com>
2023-07-10 13:41:08 -0700

    addingt testweb
    because,  why not?

---
+++ testweb/October06_cleanup/Lending/toolbar.js
@@ -0,0 +1,8 @@
+document.write('<ul>');
+document.write('<li><a href="http://www.wsulibs.wsu.edu/">Libraries Home</a></li>');
+document.write('<li><a href="http://washingtonstate.worldcat.org/">WSU Worldcat</a></li>');
+document.write('<li><a href="http://griffin.wsu.edu/search/">Griffin Catalog</a></li>');
+document.write('<li><a href="http://www.wsulibs.wsu.edu/general/research.html">Research</a></li>');
+document.write('<li><a href="http://www.wsulibs.wsu.edu/general/services.html">Services</a></li>');
+document.write('<li><a href="http://www.wsulibs.wsu.edu/index2.htm">About Us</a></li>');
+document.write('</ul>');
---
+++ testweb/October06_cleanup/pul/NEWFooter.js
@@ -0,0 +1,6 @@
+document.write ('</div>');
+document.write ('  <div id="localfooter"> <a href="#">WSU Libraries </a>, PO Box 645610, Washington State University, Pullman WA 99164-5610, 509-335-9671, <a href="mailto:libweb@wsu.edu">Contact Us</a> </div>');
+document.write ('</div>');
+document.write ('<!--Beginning of the WSU Global Footer Zone. Refer to www.wsu.edu/identity/ for more information. -->');
+var strFooter;strFooter = getFooter();document.write(strFooter);
+document.write ('<!--End of the WSU Global Footer Zone. -->');
